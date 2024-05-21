3fa7585f8c8627c0b40745371a75aa787efc1d0d
v-nazionale <164510579+v-nazionale@users.noreply.github.com>
2024-05-21 16:25:05 +0200

    Add files via upload

---
+++ docs/2024/ja/data.js
@@ -0,0 +1,41 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-7022345289_baecf14ea7_o",
+      "name": "7022345289_baecf14ea7_o",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.9389190928507283,
+          "pitch": -0.4428995456799605,
+          "title": "Ippopotamo",
+          "text": "&lt;div class=\"sketchfab-embed-wrapper\"&gt; &lt;iframe title=\"Hippopotamus statue\" frameborder=\"0\" allowfullscreen mozallowfullscreen=\"true\" webkitallowfullscreen=\"true\" allow=\"autoplay; fullscreen; xr-spatial-tracking\" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src=\"https://sketchfab.com/models/49387e3641d34473a0f48ea07c37da60/embed\"&gt; &lt;/iframe&gt; &lt;p style=\"font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;\"&gt; &lt;a href=\"https://sketchfab.com/3d-models/hippopotamus-statue-49387e3641d34473a0f48ea07c37da60?utm_medium=embed&amp;utm_campaign=share-popup&amp;utm_content=49387e3641d34473a0f48ea07c37da60\" target=\"_blank\" rel=\"nofollow\" style=\"font-weight: bold; color: #1CAAD9;\"&gt; Hippopotamus statue &lt;/a&gt; by &lt;a href=\"https://sketchfab.com/mauricesvay?utm_medium=embed&amp;utm_campaign=share-popup&amp;utm_content=49387e3641d34473a0f48ea07c37da60\" target=\"_blank\" rel=\"nofollow\" style=\"font-weight: bold; color: #1CAAD9;\"&gt; Maurice Svay &lt;/a&gt; on &lt;a href=\"https://sketchfab.com?utm_medium=embed&amp;utm_campaign=share-popup&amp;utm_content=49387e3641d34473a0f48ea07c37da60\" target=\"_blank\" rel=\"nofollow\" style=\"font-weight: bold; color: #1CAAD9;\"&gt;Sketchfab&lt;/a&gt;&lt;/p&gt;&lt;/div&gt;"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
